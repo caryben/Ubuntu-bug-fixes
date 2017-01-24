579ab314682cf6dbf6aef727afe58f89fa590124
caryben <bencary@mit.edu>
2017-01-24 13:50:10 -0500

    Update hidden_network_workaround.py

--- hidden_network_workaround.py
+++ hidden_network_workaround.py
@@ -1,3 +1,6 @@
+# My computer was failing to recognize wifi networks after being woken up from sleep so this uses the network manager command
+# line tool to force my computer to recognize the network I type in to the terminal.
+
 import subprocess
 network_name = raw_input("What is the name of your network? ")
 subprocess.check_call(['nmcli', 'c', 'up', 'id', network_name])
