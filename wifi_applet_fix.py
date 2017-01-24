ea0ed6a2e1301bbe60e5c0c48a08dfeaaea8fde8
caryben <bencary@mit.edu>
2017-01-24 13:52:34 -0500

    Update wifi_applet_fix.py

--- wifi_applet_fix.py
+++ wifi_applet_fix.py
@@ -1,3 +1,6 @@
+# this just resets the wifi symbol at the top of the screen if it's not working properly. Sometimes when I start up Ubuntu on 
+# my laptop it doesn't appear at all so this corrects that instead of me having to restart my computer.
+
 import subprocess, sys
 subprocess.check_call(['killall', 'nm-applet'])
 subprocess.check_call(['setsid', 'nm-applet'])
